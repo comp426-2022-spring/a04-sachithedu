cc6ada551a44172cf4d110827fd172cb0b7e1eb2
Sachith <sachith.iyengar@gmail.com>
2022-05-02 00:22:41 -0400

    db

--- database.js
+++ database.js
@@ -1,9 +1,7 @@
 "use strict";
 
 const Database = require('better-sqlite3');
-
-const db = new Database('log.db');
-
+const newDataBase = new Database('log.db');
 const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
 let row = stmt.get();
 
